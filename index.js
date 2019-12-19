3119c9480aa4b5e355c11a121ba9a44fec71cea6
neelavaishnavi <neelavaishnavi9898@gmail.com>
2019-12-19 14:33:25 +0530

    index

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port=process.env.PORT||3000
 app.use(express.static('public'))
-
+
 app.get('/', (req, res) => res.sendFile(__dirname + '/public/html/index.html'))
 
 app.listen(port ,()=>console.log(`example app listening on port ${port}!`))

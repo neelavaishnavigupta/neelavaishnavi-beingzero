61c259e67f0059601bfe7c3387194d29dd2bfa7a
neelavaishnavi <neelavaishnavi9898@gmail.com>
2019-12-19 11:59:30 +0530

    addingggggg

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const app = express();
 const port=process.env.PORT||3000
-app.get('/', function(req, res){
-   res.send("Hello world!");
-});
+app.get('/', (req, res)=>
+   res.send("Hello world!"))
 
-app.listen(3000);
+app.listen(port ,()=>console.log(`example app listening on port ${port}!`))

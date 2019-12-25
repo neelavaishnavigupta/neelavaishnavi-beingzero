f15ebea749aae0f3e2342621046657c0e7124ea2
neelavaishnavi <neelavaishnavi9898@gmail.com>
2019-12-25 19:28:57 +0530

    create resume

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const port=process.env.PORT||3000
-app.use(express.static('private'))
+app.use(express.static('public'))
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/private/html/index.html'))
+app.get('/', (req, res) => res.sendFile(__dirname + '/public/html/resume.html'))
 
 app.listen(port ,()=>console.log(`example app listening on port ${port}!`))
